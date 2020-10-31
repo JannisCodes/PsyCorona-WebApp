eda2111dd5e760044021230956648a4869a1aca3
Joshua Krause <joshuakrause@t-online.de>
2020-10-31 17:57:20 +0100

    Removed merge conflicts

--- data_prep_shiny_long.R
+++ data_prep_shiny_long.R
@@ -27,7 +27,7 @@ colnames(shiny_prep)[colnames(shiny_prep) %in% c("StartDate","EndDate")] <- past
 
 waves <- c("w0","w1", "w2", "w3", "w4", "w5",
            "w6", "w7", "w8", "w9", "w10",
-           "w11","w12","w13","w14")
+           "w11")
 
 ### combine waves with varnames ###
 
